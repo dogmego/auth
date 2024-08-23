6cf631e7d5a7f95d7c2fff0dc974a6332d074311
dogmego <severtor2@ya.ru>
2024-08-23 17:50:33 +0300

    innitial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	// notev1 "github.com/dogmego/MicroservicesPractice/Auth/pkg/note_v1"
+	"github.com/fatih/color"
+)
+
+const grpc = 50051
+
+type server struct {
+	//UnimplementedNoteV1Server
+}
+
+func main() {
+	fmt.Println(color.GreenString("sss"))
+}
